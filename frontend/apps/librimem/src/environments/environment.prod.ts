f52079c33d9763f5679757c41f690cb658c94a29
Erinbro <erinbro66@gmail.com>
2023-02-28 14:03:14 +0100

    Added server name to nginx conf

--- frontend/apps/librimem/src/environments/environment.prod.ts
+++ frontend/apps/librimem/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // TODO Change to https with Cert-Manager
-  bookAPI: "https://book/api/v1/books",
-  chapterAPI: "https://chapter/api/v1/chapters",
-  userAPI: "https://user/api/v1/users"
+  bookAPI: "https://www.librimem.online/api/v1/books",
+  chapterAPI: "https://www.librimem.online/api/v1/chapters",
+  userAPI: "https://www.librimem.online/api/v1/users"
 };
